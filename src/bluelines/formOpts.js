fd2365f5bae14cbe93d7889eafd766e4576fa102
Glitch (instinctive-metal) <none>
2019-02-12 07:40:27 +0000

    🕰🐾 Update method collection

--- src/bluelines/formOpts.js
+++ src/bluelines/formOpts.js
@@ -20,7 +20,7 @@ module.exports = function formOpts(input) {
       basicDisplay: "",
       groupDisplay: "",
     }
-    console.log(bellgroup);
+    //console.log(bellgroup);
     return formInfo;
   }
   
