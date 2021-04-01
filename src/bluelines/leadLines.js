4ff8241adbe105ec97009d874381f1ab4a415d2c
Glitch (orchestrali-instinctive-metal) <none>
2021-04-01 21:31:00 +0000

    💝🎠 clean up

--- src/bluelines/leadLines.js
+++ src/bluelines/leadLines.js
@@ -1,11 +1,11 @@
 
-
+//this is not actually the rowArray, but an array of rownums that have names (leadhead, half lead, new six)
 module.exports = function buildLines(width, rowArray, x) {
 
   //line under the first leadhead
   let lines = `<g style="stroke: #111; stroke-width:1;">
 <line x1="${x}" y1="20" x2="` + width + `" y2="20" />`
-  //every leadLength rows draw a line
+  
   for (let i = 0; i < rowArray.length; ++i) {
     let lineY = rowArray[i]*20;
     
--- src/method/callPos.js
+++
@@ -1,5 +0,0 @@
-const calls = [1,'i','o','f','v','x','s','e','n'];
-
-module.exports = function calls(stage) {
-  
-}
